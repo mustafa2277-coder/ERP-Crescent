0c2b75511fecd2373128f67b998348f735d25f44
Khurshid Imran <khurshidimran14@gmail.com>
2018-01-11 13:55:40 +0500

    working of ledger tree

--- routes/web.php
+++ routes/web.php
@@ -89,5 +89,7 @@ Route::get('/getGeneralLedgerView','ReportController@GetGeneralLedgerView')->mid
 Route::get('/getFilterGeneralLedgerList' ,'ReportController@GetFilterGeneralLedgerList')->middleware('auth');
 
 
+Route::get('/getFilterGeneralLedgerList2' ,'ReportController@GetFilterGeneralLedgerList2')->middleware('auth');
+
 
 
